fc7b9717ae33e220b0934115dd3310a35790de37
saadfarooq005 <39597457+saadfarooq005@users.noreply.github.com>
2020-05-13 10:47:00 +0200

    Add files via upload

---
+++ String Examples/String Examples.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+String s;
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available())
+  {
+    s = Serial.readString();
+    Serial.println(s);
+    Serial.println(s.startsWith("+CMT"));
+  }
+}
